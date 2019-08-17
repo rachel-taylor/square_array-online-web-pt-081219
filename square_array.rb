fc04b279e9c1dea0a78d37c0ee1dee882cab0060
Rachel T <rachmaybe@gmail.com>
2019-08-17 20:11:20 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -4,7 +4,7 @@ def square_array(array)
   placeholder = []
   array.each do |i|
   placeholder << i ** 2 
-  # binding.pry
+   binding.pry
   # placeholder << array.each {|array| array ** 2}
   # counter = 0 
     # while numbers 
