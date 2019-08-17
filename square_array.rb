27b8c4eea472da321de5e970ff100ab718f0359a
Rachel T <rachmaybe@gmail.com>
2019-08-17 19:50:54 +0000

    Done.

--- square_array.rb
+++ square_array.rb
@@ -1,3 +1,19 @@
+require "pry"
+
 def square_array(array)
-  # your code here
+  placeholder = []
+  array.each do |i|
+  placeholder << i ** 2 
+  # binding.pry
+  # placeholder << array.each {|array| array ** 2}
+  # counter = 0 
+    # while numbers 
+      # placeholder << (array[counter]**numbers[counter])
+      # counter +=1 
+     end 
+    return placeholder
 end
+
+
+
+
